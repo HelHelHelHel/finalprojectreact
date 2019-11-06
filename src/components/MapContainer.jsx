0d051f72f8ec9b32af53c7cbc333b16971a95b7f
Hel <helenahrubesova80@gmail.com>
2019-11-06 09:00:16 +0100

    first commit

---
+++ src/components/MapContainer.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+  };
+  
+  export class MapContainer extends Component {
+    render() {
+      return (
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+           lat: 50.0755,
+           lng: 14.4378
+          }}>
+          <Marker position={{ lat: 50.083, lng: 14.45}} />
+        </Map>
+      );
+    }
+  }
+  
+  export default GoogleApiWrapper({
+    apiKey: 'AIzaSyCdWaaJdA2vNPKHCJlErt7c5PKktVn2ro8'
+  })(MapContainer);
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MapContainer from './components/MapContainer.jsx';
+
+import './index.html';
+
+ReactDOM.render(<MapContainer/>, document.getElementById('app'));
