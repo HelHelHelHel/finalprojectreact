1b5a81e89c48c1a33d5c63add1726fa7f6f9d2af
Hel <helenahrubesova80@gmail.com>
2019-11-07 16:31:25 +0100

    infowindow

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -12,9 +12,24 @@ const mapStyles = {
         this.state = {
           showingInfoWindow: false,  //Hides or the shows the infoWindow
           activeMarker: {},          //Shows the active marker upon click
-          selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+          selectedPlace: {}         //Shows the infoWindow to the selected place upon a marker
         };
       }
+      onMarkerClick = (props, marker) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+  
+    onClose = props => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+    };
     render() {
       return (
         <Map
@@ -25,7 +40,17 @@ const mapStyles = {
            lat: 50.0755,
            lng: 14.4378
           }}>
-          <Marker position={{ lat: 50.083, lng: 14.45}} />
+          <Marker onClick={this.onMarkerClick} position={{ lat: 50.083, lng: 14.45}} />
+          
+          <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}>
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+          </InfoWindow>
+
         </Map>
       );
     }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MapContainer from './components/MapContainer.jsx';
+import App from './components/App.jsx';
 
 import './index.html';
 
-ReactDOM.render(<MapContainer/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
