e9fed4405fae70c533d4d74efef579cd58948b85
garciazapiain <juangarciazapiain@gmail.com>
2019-11-13 17:52:53 +0100

    geo

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,60 +1,61 @@
-import React, {Component} from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import React,  {Component} from 'react';
+import { GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import { googleKey } from '../../key.js';
+import CurrentLocation from './Map.jsx'
 const mapStyles = {
     width: '100%',
     height: '100%'
   };
-  
-  export class MapContainer extends Component {
+
+  class MapContainer extends Component {
+      
       constructor(props) {
         super(props);
+        
         this.state = {
-          showingInfoWindow: false,  //Hides or the shows the infoWindow
-          activeMarker: {},          //Shows the active marker upon click
-          selectedPlace: {}         //Shows the infoWindow to the selected place upon a marker
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {}
+          }
         };
-      }
-      onMarkerClick = (props, marker) =>
+      
+
+    onMarkerClick = (props, marker, e) =>
       this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
       });
+    }
+  };
   
-    onClose = props => {
-      if (this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        });
-      }
-    };
-    render() {
-      return (
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={{
-           lat: 50.0755,
-           lng: 14.4378
-          }}>
-          <Marker onClick={this.onMarkerClick} position={{ lat: 50.083, lng: 14.45}} />
-          
-          <InfoWindow
+  render() {
+    return (
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
-          onClose={this.onClose}>
+          onClose={this.onClose}
+        >
           <div>
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
-          </InfoWindow>
-
-        </Map>
-      );
-    }
+        </InfoWindow>
+      </CurrentLocation>
+    );
   }
+}
   
   export default GoogleApiWrapper({
     apiKey: googleKey
